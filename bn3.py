4f4f3e703db3636f4f2642bbb01350efb3548bb6
Denis Alonso <denisalonso1999@gmail.com>
2024-06-06 20:43:31 -0300

    Update bn3.py

--- bn3.py
+++ bn3.py
@@ -1,3 +1,4 @@
+
 def cria_mapa():
     mapa = []
     for i in range(10):
@@ -10,6 +11,7 @@ def cria_mapa():
 m1 = cria_mapa()
 m2 = cria_mapa()
 
+
 letters = ['A','B','C','D','E','F','D','H','I','J']
 numbers = ['1','2','3','4','5','6','7','8','9','10']
 
@@ -18,7 +20,7 @@ for i in range(12):
         line = '   '
         for i in range(20):
             if i in [10]:
-                line += '      '
+                line += '       '
             if i > 9:
                 line += f'  {letters[i-10]}  '
             else: 
@@ -26,13 +28,23 @@ for i in range(12):
     else:
         line = ''
         for h in range(24):
-            if h in [0,11,12,23]:
+            if h in [0,12]:
                 if i in [10]:
                     line += numbers[i-1]
                     line += ' '              
                 else:
                     line += ' '
                     line += numbers[i-1]
+                    line += ' '                
+            elif h in [11,23]:
+                line += ' '
+                if i in [10]:
+                    line += numbers[i-1]
+                    line += ' '              
+                else:
+                    
+                    line += numbers[i-1]
+                    line += ' '
                     line += ' '
             elif h < 10:
                 line += m1[i-1][h]
