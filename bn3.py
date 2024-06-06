96e4ef6807e6d60f8bfd3411c9a2e609bcaa9a96
Denis Alonso <denisalonso1999@gmail.com>
2024-06-06 20:30:35 -0300

    Add files via upload

---
+++ bn3.py
@@ -0,0 +1,53 @@
+
+def cria_mapa():
+    mapa = []
+    for i in range(10):
+        mapa.append([])
+    for i in range(10):
+        for j in range(10):
+            mapa[i].append('     ')
+    return mapa
+
+m1 = cria_mapa()
+m2 = cria_mapa()
+
+
+# legenda_cima = '   '
+# for i in range(20):
+#     if i in [10]:
+#         legenda_cima += '      '
+#     if i > 9:
+#         legenda_cima += f'  {letters[i-10]}  '
+#     else: 
+#         legenda_cima += f'  {letters[i-10]}  '
+# print(legenda_cima)
+
+letters = ['A','B','C','D','E','F','D','H','I','J']
+numbers = ['1','2','3','4','5','6','7','8','9','10']
+
+for i in range(12):
+    if i in [0,11]:
+        line = '   '
+        for i in range(20):
+            if i in [10]:
+                line += '      '
+            if i > 9:
+                line += f'  {letters[i-10]}  '
+            else: 
+                line += f'  {letters[i-10]}  '
+    else:
+        line = ''
+        for h in range(24):
+            if h in [0,11,12,23]:
+                if i in [10]:
+                    line += numbers[i-1]
+                    line += ' '              
+                else:
+                    line += ' '
+                    line += numbers[i-1]
+                    line += ' '
+            elif h < 10:
+                line += m1[i-1][h]
+            else:
+                line += m2[i-1][h-13]
+    print(line)
