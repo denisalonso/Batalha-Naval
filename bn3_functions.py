8f6242789daec15f7d2f03d7b9623a6a265d923c
Denis Alonso <denisalonso1999@gmail.com>
2024-06-07 00:45:52 -0300

    Update bn3_functions.py

--- bn3_functions.py
+++ bn3_functions.py
@@ -10,7 +10,7 @@ def cria_mapa():
     return mapa
 
 def show_map(m1,m2):
-    letters = ['A','B','C','D','E','F','D','H','I','J']
+    letters = ['A','B','C','D','E','F','G','H','I','J']
     numbers = ['1','2','3','4','5','6','7','8','9','10']
     for i in range(12):
         if i in [0,11]:
@@ -43,8 +43,8 @@ def show_map(m1,m2):
                         line += numbers[i-1]
                         line += ' '
                         line += ' '
-                elif h < 10:
-                    line += m1[i-1][h]
+                elif h <= 10:
+                    line += m1[i-1][h-1]
                 else:
                     line += m2[i-1][h-13]
         print(line)
@@ -128,3 +128,26 @@ def vivo(m):
                 alive = True
                 break
     return alive
+
+def atira_cpu(m,coords_atiradas):
+    coords_validas = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10',
+                    'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10',
+                    'c1','c2','c3','c4','c5','c6','c7','c8','c9','c10',
+                    'd1','d2','d3','d4','d5','d6','d7','d8','d9','d10',
+                    'e1','e2','e3','e4','e5','e6','e7','e8','e9','e10',
+                    'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10',
+                    'g1','g2','g3','g4','g5','g6','g7','g8','g9','g10',
+                    'h1','h2','h3','h4','h5','h6','h7','h8','h9','h10',
+                    'i1','i2','i3','i4','i5','i6','i7','i8','i9','i10',
+                    'j1','j2','j3','j4','j5','j6','j7','j8','j9','j10',]
+    letras = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10}
+    bala = rd.choice(coords_validas)
+    while bala in coords_atiradas:
+        bala = rd.choice(coords_validas)
+    l = int(bala[1:])-1
+    col = letras[bala[0]]-1
+    if m[l][col] == '     ':
+        m[l][col] = 'water'
+    elif m[l][col] == 'navio':
+        m[l][col] = 'booom'
+    return [m,bala]
